708a02f9cee43c23cb785b92d69129ac329b2b02
KOIZUMIharuto <koizumi-haruto@ed.tmu.ac.jp>
2024-03-01 20:56:50 +0900

    update push_swap

--- push_swap/srcs/push_swap.c
+++ push_swap/srcs/push_swap.c
@@ -6,7 +6,7 @@
 /*   By: hkoizumi <hkoizumi@student.42.jp>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/01 19:21:27 by hkoizumi          #+#    #+#             */
-/*   Updated: 2024/03/01 19:55:05 by hkoizumi         ###   ########.fr       */
+/*   Updated: 2024/03/01 20:52:33 by hkoizumi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,4 +14,7 @@
 
 void	push_swap(t_node *stack_a, t_node *stack_b, int node_count)
 {
+	(void)stack_a;
+	(void)stack_b;
+	(void)node_count;
 }
