1b48a5f8c17904970a957f52f6c77debd827d7b0
KOIZUMIharuto <105192235+KOIZUMIharuto@users.noreply.github.com>
2024-08-28 14:51:19 +0900

    accept over 2 cmd

--- pipex/srcs/main.c
+++ pipex/srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: hkoizumi <hkoizumi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/09 10:49:39 by hkoizumi          #+#    #+#             */
-/*   Updated: 2024/08/28 14:41:09 by hkoizumi         ###   ########.fr       */
+/*   Updated: 2024/08/28 14:49:19 by hkoizumi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,13 +16,13 @@ int	main(int argc, char *argv[], char *envp[])
 {
 	t_vars	vars;
 
-	if (argc != 5)
+	if (argc < 5)
 		error_exit(NULL, USAGE_ERROR);
 	vars = (t_vars){-1, -1, -1, NULL, NULL};
 	vars.infile_fd = open(argv[1], O_RDONLY);
 	if (vars.infile_fd == -1)
 		error_exit(NULL, strerror(errno));
-	vars.outfile_fd = open(argv[4], O_WRONLY | O_CREAT | O_TRUNC, 0644);
+	vars.outfile_fd = open(argv[argc - 1], O_WRONLY | O_CREAT | O_TRUNC, 0644);
 	if (vars.outfile_fd == -1)
 		error_exit(&vars, strerror(errno));
 	vars.cmds =  &argv[2];
