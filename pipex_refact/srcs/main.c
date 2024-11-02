a685c5e5c8e56124330d17c9a6b8785f94746f54
KOIZUMIharuto <105192235+KOIZUMIharuto@users.noreply.github.com>
2024-11-03 00:47:32 +0900

    change file open order

--- pipex_refact/srcs/main.c
+++ pipex_refact/srcs/main.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: hkoizumi <hkoizumi@student.42.jp>          +#+  +:+       +#+        */
+/*   By: hkoizumi <hkoizumi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/09 10:49:39 by hkoizumi          #+#    #+#             */
-/*   Updated: 2024/10/30 16:36:54 by hkoizumi         ###   ########.fr       */
+/*   Updated: 2024/11/03 00:10:04 by hkoizumi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,7 @@
 int	main(int argc, char *argv[], char *envp[])
 {
 	t_vars	vars;
+	char	*infile_open_errmsg;
 
 	if (argc < 5)
 	{
@@ -22,12 +23,14 @@ int	main(int argc, char *argv[], char *envp[])
 		exit(EXIT_FAILURE);
 	}
 	vars = (t_vars){-1, -1, -1, NULL, NULL, NULL, {-1, -1}, -1};
+	vars.infile_fd = open(argv[1], O_RDONLY);
+	if (vars.infile_fd == -1)
+		infile_open_errmsg = strerror(errno);
 	vars.outfile_fd = open(argv[argc - 1], O_WRONLY | O_CREAT | O_TRUNC, 0644);
 	if (vars.outfile_fd == -1)
 		print_msgs(strerror(errno), argv[argc - 1]);
-	vars.infile_fd = open(argv[1], O_RDONLY);
 	if (vars.infile_fd == -1)
-		print_msgs(strerror(errno), argv[1]);
+		print_msgs(infile_open_errmsg, argv[1]);
 	vars.cmds = &argv[2];
 	vars.cmds_count = argc - 3;
 	vars.envp = envp;
