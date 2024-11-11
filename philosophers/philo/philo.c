d7af5b30ff88570d9369e1edafd28fb851c32eb5
Haruto Koizumi <hkoizumi@c4r2s16.42tokyo.jp>
2024-11-11 15:24:56 +0900

    norm

--- philosophers/philo/philo.c
+++ philosophers/philo/philo.c
@@ -3,15 +3,16 @@
 /*                                                        :::      ::::::::   */
 /*   philo.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: hkoizumi <hkoizumi@student.42.fr>          +#+  +:+       +#+        */
+/*   By: hkoizumi <hkoizumi@student.42.jp>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/07 22:14:12 by hkoizumi          #+#    #+#             */
-/*   Updated: 2024/11/11 15:07:14 by hkoizumi         ###   ########.fr       */
+/*   Updated: 2024/11/11 15:23:08 by hkoizumi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <philo.h>
 
+static bool	philo_routine(t_philo *philo);
 static int	philo_eat(t_philo *philo);
 static int	take_fork(t_philo *philo);
 
@@ -44,9 +45,7 @@ int	philo_wait(t_philo *philo, long long start, long long time)
 void	*do_philo(void *arg)
 {
 	t_philo		*philo;
-	long long	sleep_start;
 	bool		*is_success;
-	bool		is_fin;
 
 	philo = (t_philo *)arg;
 	is_success = (bool *)malloc(sizeof(bool));
@@ -55,22 +54,31 @@ void	*do_philo(void *arg)
 	*is_success = false;
 	if (get_time(&philo->last_eat) || plog(philo, NULL, THINK))
 		return (is_success);
+	*is_success = philo_routine(philo);
+	return (is_success);
+}
+
+static bool	philo_routine(t_philo *philo)
+{
+	long long	sleep_start;
+	bool		is_fin;
+
+	is_fin = false;
 	while (1)
 	{
 		if (check_fin(philo->died, philo->fin, &is_fin))
-			return (is_success);
+			return (false);
 		if (is_fin)
 			break ;
 		if (philo_eat(philo))
-			return (is_success);
+			return (false);
 		if (plog(philo, &sleep_start, SLEEP)
 			|| philo_wait(philo, sleep_start, philo->time_to_sleep))
-			return (is_success);
+			return (false);
 		if (plog(philo, NULL, THINK))
-			return (is_success);
+			return (false);
 	}
-	*is_success = true;
-	return (is_success);
+	return (true);
 }
 
 static int	philo_eat(t_philo *philo)
